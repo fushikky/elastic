f846205b55fdf218c6c5e120f69cd7f786304dca
fushiki_hideki <fushiki_hideki@cyberagent.co.jp>
2017-07-31 17:30:54 +0900

    with res

--- client.go
+++ client.go
@@ -1259,6 +1259,7 @@ func (c *Client) PerformRequest(ctx context.Context, method, path string, params
 		// Get response
 		fmt.Println("DEBUGLOG:c.c.Do((*http.Request)(req).WithContext(ctx))")
 		res, err := c.c.Do((*http.Request)(req).WithContext(ctx))
+		fmt.Printf("res:%+v\n", res)
 		fmt.Println("DEBUGLOG:c.c.Do((*http.Request)(req).WithContext(ctx)):DONE")
 		if err == context.Canceled || err == context.DeadlineExceeded {
 			// Proceed, but don't mark the node as dead
